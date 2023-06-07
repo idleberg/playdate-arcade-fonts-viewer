acf9d3ae4799d0a2273110d43ff76e90c55e931d
Jan T. Sott <git@idleberg.com>
2023-06-08 00:41:22 +0200

    update build script

--- tools/build.sh
+++ tools/build.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-time pdc --verbose --strip source viewer.pdx
+
+PDX_FILE=viewer.pdx
+echo -e "\nCompiling..."
+pdc --verbose --strip source $PDX_FILE
+
+echo -e "\nCompressing..."
+rm "$PDX_FILE.zip"
+zip -r -9 "$PDX_FILE.zip" $PDX_FILE
